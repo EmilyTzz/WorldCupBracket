efe2a6b5008b610488f8271b28df30759825af6f
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-07 22:45:25 -0600

    Created Group object and added sttributes to the Team object

--- src/object/Team.java
+++ src/object/Team.java
@@ -1,4 +1,23 @@
 package object;
 
 public class Team {
+
+    private String name;
+
+    private String group;
+
+
+    public Team(String name, String group, int rank){
+        this.name = name;
+        this.group = group;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getGroup(){
+        return group;
+    }
+
 }
