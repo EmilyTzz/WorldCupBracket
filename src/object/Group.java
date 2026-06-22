4b5d30a2db5f65ff85376a30723e0dcf319b9f18
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-21 21:13:28 -0600

    Divided the teams in the groupstage based on the groups they are in

--- src/object/Group.java
+++ src/object/Group.java
@@ -1,4 +1,31 @@
 package object;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private ArrayList<Team> teams;
+
+    public Group() {
+        teams = new ArrayList<Team>();
+    }
+
+    public void addTeam(Team team){
+        teams.add(team);
+    }
+
+    @Override
+    public String toString() {
+        return teams.toString();
+    }
+
+    public int getSize(){
+        return teams.size();
+    }
+
+    public ArrayList<Team> getTeams(){
+        return teams;
+    }
+
 }
--- src/stage/GroupStage.java
+++ src/stage/GroupStage.java
@@ -4,13 +4,14 @@ import object.Group;
 import object.Team;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GroupStage {
 
-    private Team[] teams;
+    private List<Team> teams;
 
-    private Group[][] groups;
+    private Group[] groups;
 
     private List<Group> top1s;
 
@@ -18,12 +19,55 @@ public class GroupStage {
 
     private List<Group> top3s;
 
-    public GroupStage(Team teams, Group groups, int numberOfTeams){
-        this.teams = new Team[numberOfTeams];
-        this.groups = new Group[numberOfTeams/4][4];
+    private int numberOfTeams;
+
+    public GroupStage(List<Team> teams, int numberOfTeams){
+        this.teams = teams;
+        this.numberOfTeams = numberOfTeams;
+        this.groups = new Group[numberOfTeams/4];
         this.top1s = new ArrayList<>();
         this.top2s = new ArrayList<>();
         this.top3s = new ArrayList<>();
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public Group[] getGroups() {
+        return groups;
+    }
+
+    public List<Group> getTop1s() {
+        return top1s;
+    }
+
+    public List<Group> getTop2s() {
+        return top2s;
+    }
+
+    public List<Group> getTop3s() {
+        return top3s;
+    }
+
+    public Group[] sortGroups() {
+        for (int i = 0; i < numberOfTeams / 4; i++) {
+            Group group = new Group();
+            group.addTeam(teams.get(0));
+            for (int k = 1; k < teams.size(); k++) {
+                if (teams.get(0).getGroup().equals(teams.get(k).getGroup())) {
+                    group.addTeam(teams.get(k));
+                }
+                if (group.getSize() == 4) {
+                    for (Team team : group.getTeams()){
+                        teams.remove(team);
+                    }
+                    break;
+                }
+            }
+            groups[i] = group;
+        }
+        return groups;
     }
 
 }
