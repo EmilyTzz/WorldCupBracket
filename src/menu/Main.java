be6fcd5ac57b2b870d40eda1c007a9d1c5d01a03
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-21 21:16:15 -0600

    Displayed all the groups in the menu

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -2,8 +2,21 @@ package menu;
 
 import menu.Menu;
 
+import java.io.File;
+
 public class Main{
     public static void main(String args[]){
-        Menu.mainMenu();
+        if (args.length > 1){
+            System.out.println("Invalid Number of Arguments");
+        }
+        else if(args.length == 1){
+            File file = new File(args[0]);
+            if (!file.exists()){
+                System.out.println("File Does Not Exist");
+            }
+            else{
+                Reader.loadTeams(file);
+            }
+        }
     }
 }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,8 +1,30 @@
 package menu;
 
+import object.Group;
+import object.Team;
+import stage.GroupStage;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Menu {
 
-    public static void mainMenu(){
-        System.out.println("Menu");
+    public static void mainMenu(List<Team> teams){
+        System.out.println("----------------------------Group Stage----------------------------\n");
+        GroupStage groupStage = new GroupStage(teams, teams.size());
+        Group[] groups = groupStage.sortGroups();
+        //System.out.println(Arrays.toString(groups));
+        displayGroup(groups);
     }
+
+    private static void displayGroup(Group[] group){
+        for (int i = 0; i < group.length; i++){
+            String groupName = group[i].getTeams().get(0).getGroup();
+            System.out.println("\n-----------------------"+groupName+"-----------------------");
+            for (int j = 0; j < group[i].getSize(); j ++){
+                System.out.println("- " + group[i].getTeams().get(j).getName());
+            }
+        }
+    }
+
 }
