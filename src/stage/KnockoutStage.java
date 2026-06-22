3af6f021aec5109440265adac700b8ac022bf1fd
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-22 14:28:54 -0600

    Added getters to knockout stage and an attribute that stores all the teams in order from strongest to weakest

--- src/stage/KnockoutStage.java
+++ src/stage/KnockoutStage.java
@@ -16,14 +16,30 @@ public class KnockoutStage {
 
     private ArrayList<Team> rightSide;
 
+    private ArrayList<Team> allTeams;
+
     public KnockoutStage(ArrayList<Team> top1s, ArrayList<Team> top2s, ArrayList<Team> top3s){
         this.top1s = new ArrayList<>(top1s);
         this.top2s = new ArrayList<>(top2s);
         this.top3s = new ArrayList<>(top3s);
+        this.allTeams = new ArrayList<>();
+        // Adds all the teams in the order of top 1 to top 3
+        allTeams.addAll(top1s);
+        allTeams.addAll(top2s);
+        allTeams.addAll(top3s);
         this.leftSide = new ArrayList<>();
         this.rightSide = new ArrayList<>();
     }
 
+    public ArrayList<Team> getAllTeams(){
+        return allTeams;
+    }
 
+    public ArrayList<Team> getLeftSide() {
+        return leftSide;
+    }
 
+    public ArrayList<Team> getRightSide() {
+        return rightSide;
+    }
 }
