b2a33b26849ae62ad5cbf29166f6fcb6157ea559
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-22 11:58:40 -0600

    Added attributes to knockout stage

--- src/stage/KnockoutStage.java
+++ src/stage/KnockoutStage.java
@@ -1,4 +1,29 @@
 package stage;
 
+import object.Team;
+
+import java.util.ArrayList;
+
 public class KnockoutStage {
+
+    private ArrayList<Team> top1s;
+
+    private ArrayList<Team> top2s;
+
+    private ArrayList<Team> top3s;
+
+    private ArrayList<Team> leftSide;
+
+    private ArrayList<Team> rightSide;
+
+    public KnockoutStage(ArrayList<Team> top1s, ArrayList<Team> top2s, ArrayList<Team> top3s){
+        this.top1s = new ArrayList<>(top1s);
+        this.top2s = new ArrayList<>(top2s);
+        this.top3s = new ArrayList<>(top3s);
+        this.leftSide = new ArrayList<>();
+        this.rightSide = new ArrayList<>();
+    }
+
+
+
 }
