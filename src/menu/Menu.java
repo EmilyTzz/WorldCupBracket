ad513ba1867f5ce158e5d4ca647d1a1b2452041a
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-06-23 10:31:22 -0600

    added a function to display the matches in the round of 32

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,17 +1,16 @@
 package menu;
 
 import object.Group;
+import object.KnockoutMatchesDivider;
+import object.Match;
 import object.Team;
 import stage.GroupStage;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Menu {
 
-    public static void mainMenu(List<Team> teams){
+    public static void mainMenu(List<Team> teams) {
         System.out.println("\n----------------------------Group Stage----------------------------\n");
         GroupStage groupStage = new GroupStage(teams, teams.size());
         Group[] groups = groupStage.sortGroups();
@@ -19,12 +18,12 @@ public class Menu {
         displayGroup(groups, groupStage);
     }
 
-    private static void displayGroup(Group[] group, GroupStage groupStage){
-        for (int i = 0; i < group.length; i++){
+    private static void displayGroup(Group[] group, GroupStage groupStage) {
+        for (int i = 0; i < group.length; i++) {
             String groupName = group[i].getTeams().get(0).getGroup();
-            System.out.println("\n-----------------------"+groupName+"-----------------------");
-            for (int j = 0; j < group[i].getSize(); j ++){
-                System.out.println(j+1 + ". " + group[i].getTeams().get(j).getName());
+            System.out.println("\n-----------------------" + groupName + "-----------------------");
+            for (int j = 0; j < group[i].getSize(); j++) {
+                System.out.println(j + 1 + ". " + group[i].getTeams().get(j).getName());
             }
             int firstPlace;
             while (true) {
@@ -49,8 +48,7 @@ public class Menu {
                     if (secPlace > 0 && secPlace < 5 && secPlace != firstPlace) {
                         groupStage.addTop2s(group[i].getTeams().get(secPlace - 1));
                         break;
-                    }
-                    else if (secPlace == firstPlace){
+                    } else if (secPlace == firstPlace) {
                         System.out.println("The team is already selected");
                     }
                 } catch (InputMismatchException e) {
@@ -66,8 +64,7 @@ public class Menu {
                     if (thirdPlace > 0 && thirdPlace < 5 && thirdPlace != firstPlace && thirdPlace != secPlace) {
                         groupStage.addTop3s(group[i].getTeams().get(thirdPlace - 1));
                         break;
-                    }
-                    else if (thirdPlace == firstPlace || thirdPlace == secPlace){
+                    } else if (thirdPlace == firstPlace || thirdPlace == secPlace) {
                         System.out.println("The team is already selected");
                     }
                 } catch (InputMismatchException e) {
@@ -87,7 +84,7 @@ public class Menu {
             try {
                 for (int i = 0; i < 8; i++) {
                     Scanner scanner = new Scanner(System.in);
-                    System.out.print("Top " + (i+1) + " Third Place Team: ");
+                    System.out.print("Top " + (i + 1) + " Third Place Team: ");
                     int option = scanner.nextInt();
                     if (option < 13 && option > 0 && groupStage.top3sContains(groupStage.getTop3s().get(option - 1)) == false) {
                         groupStage.addBestTop3s(groupStage.getTop3s().get(option - 1));
@@ -101,36 +98,48 @@ public class Menu {
         displayAllTopTeams(groupStage);
     }
 
-    private static void displayAllTopTeams(GroupStage groupStage){
+    private static void displayAllTopTeams(GroupStage groupStage) {
         System.out.println("\n-----------------------Top 1s-----------------------");
-        for (int i = 0; i < groupStage.getTop1s().size(); i++){
-            System.out.println(i+1 + ". " + groupStage.getTop1s().get(i));
+        for (int i = 0; i < groupStage.getTop1s().size(); i++) {
+            System.out.println(i + 1 + ". " + groupStage.getTop1s().get(i));
         }
         System.out.println("\n-----------------------Top 2s-----------------------");
-        for (int i = 0; i < groupStage.getTop2s().size(); i++){
-            System.out.println(i+1 + ". " + groupStage.getTop2s().get(i));
+        for (int i = 0; i < groupStage.getTop2s().size(); i++) {
+            System.out.println(i + 1 + ". " + groupStage.getTop2s().get(i));
         }
         System.out.println("\n-----------------------Top 3s-----------------------");
-        for (int i = 0; i < groupStage.getBestTop3s().size(); i++){
-            System.out.println(i+1 + ". " + groupStage.getBestTop3s().get(i));
+        for (int i = 0; i < groupStage.getBestTop3s().size(); i++) {
+            System.out.println(i + 1 + ". " + groupStage.getBestTop3s().get(i));
         }
         boolean isRunning = true;
-        while (isRunning){
+        while (isRunning) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("\nClick N to move to the next round... (X to exit)");
+            System.out.print("\nClick N to move to the next round... (X to exit): ");
             String option = scanner.nextLine();
-            if (option.toUpperCase().equals("X")){
+            if (option.toUpperCase().equals("X")) {
                 exitBracket();
-            }
-            else if (option.toUpperCase().equals("N")){
+            } else if (option.toUpperCase().equals("N")) {
                 bestOf32(groupStage);
                 isRunning = false;
             }
         }
     }
 
-    private static void bestOf32(GroupStage groupStage){
+    private static void bestOf32(GroupStage groupStage) {
         System.out.println("\n----------------------------Best of 32----------------------------\n");
+        ArrayList<Team> bestOf32teams = new ArrayList<>();
+        bestOf32teams.addAll(groupStage.getTop1s());
+        bestOf32teams.addAll(groupStage.getTop2s());
+        bestOf32teams.addAll(groupStage.getBestTop3s());
+        KnockoutMatchesDivider knockoutMatchesDivider = new KnockoutMatchesDivider(bestOf32teams, 32);
+        knockoutMatchesDivider.selectLeftSideTeams();
+        knockoutMatchesDivider.selectRightSideTeams();
+        //System.out.println(new ArrayList<>(knockoutMatchesDivider.getLeftSide()));
+        //System.out.println(new ArrayList<>(knockoutMatchesDivider.getRightSide()));
+        Match[] leftSideMatches = knockoutMatchesDivider.getMatches(new ArrayList<>(knockoutMatchesDivider.getLeftSide()));
+        Match[] rightSideMatches = knockoutMatchesDivider.getMatches(new ArrayList<>(knockoutMatchesDivider.getRightSide()));
+        System.out.println(Arrays.toString(leftSideMatches));
+        System.out.println(Arrays.toString(rightSideMatches));
     }
 
     private static void exitBracket(){
